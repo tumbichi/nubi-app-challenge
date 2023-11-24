d527192e3845f10c045085120e16fdfd8614e0b2
Tumbichi <germanviglietti@gmail.com>
2023-11-24 14:29:24 -0300

    build: avoid import React on .tsx files and config '@' as a global alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+        cwd: 'babelrc',
+      },
+    ],
+  ],
 };
