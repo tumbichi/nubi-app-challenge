cc559362965b50882bcce0f83aa2c8a316b74bc6
Tumbichi <germanviglietti@gmail.com>
2023-11-26 16:31:59 -0300

    feat: install and config i18n with auth resources

---
+++ src/Base/i18n/index.ts
@@ -0,0 +1,40 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+import authES from './locales/es/auth.json';
+import commonES from './locales/es/common.json';
+
+import authEN from './locales/en/auth.json';
+import commonEN from './locales/en/common.json';
+
+const es = {
+  auth: authES,
+  common: commonES,
+};
+
+const en = {
+  auth: authEN,
+  common: commonEN,
+};
+
+export const defaultNS = 'common';
+
+export const resources = {
+  es,
+  en,
+} as const;
+
+i18n.use(initReactI18next).init({
+  resources,
+  defaultNS,
+  lng: 'es',
+  ns: ['common', 'auth'],
+  fallbackLng: 'es',
+  react: {
+    nsMode: 'fallback',
+  },
+  compatibilityJSON: 'v3',
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+});
