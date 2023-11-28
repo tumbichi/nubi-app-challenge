1080c58ad52f357707ab6d7252abc59671ed8e57
Tumbichi <germanviglietti@gmail.com>
2023-11-28 17:30:46 -0300

    chore: remove unused code

--- src/Card/components/ExpenseItem.tsx
+++ src/Card/components/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import React from 'react';
 import {Summary} from '@/Auth/data/AuthRepository';
 import formatPrice from '@/Base/utils/formatters/formatPrice';
